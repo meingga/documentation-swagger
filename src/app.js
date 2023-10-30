bbd90fdfe70468a7a944109c20e0869fd2a32b67
fahmifaqih <fahmi.nutech@gmail.com>
2023-10-30 16:28:34 +0700

    add express basic auth

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
+const basicAuth = require('express-basic-auth');
 
 const swaggerDocument = require('../docs/api.json');
 
 const app = express();
 const port = process.env.PORT || 3000;
 const base_url = process.env.URL || "http://localhost";
-
+const verifyAuth = basicAuth({
+    users: { 'fahmifaqih': 'abbcccdddd' },
+    challenge: true,
+});
 // Menggunakan Swagger UI untuk menampilkan dokumentasi
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/docs', verifyAuth, swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // default
 app.use((req, res) => {
